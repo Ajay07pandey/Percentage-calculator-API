47f059c4470a8525f709950d963f9de00f546c94
Ajay07pandey <whoajaypandey@gmail.com>
2023-10-04 17:26:58 +0530

    Percent Calculator

---
+++ Project1.py
@@ -0,0 +1,52 @@
+from flask import Flask,render_template,request,redirect,url_for
+
+## create the flask app
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h2>Hello, World!</h2>"
+
+@app.route('/welcome')
+def welcome():
+    return "Welcome to the Flask Tutorials"
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "the person is passed and the score is "+str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "the person has failed and the score is "+str(score)
+
+
+@app.route('/calculate',methods=['POST','GET'])
+def calculate():
+    if request.method=='GET':
+        return render_template('calculate.html')
+    else:
+        maths= float(request.form['maths'])
+        chemistry =float(request.form['chemistry'])
+        physics =float(request.form['physics'])
+        computer_science =float(request.form['computer_science'])
+        hindi =float(request.form['hindi'])
+
+        average_marks=(maths+chemistry+physics+computer_science+hindi)/5
+
+        result=""
+        if average_marks>=50:
+            result="success"
+        else:
+            result="fail"
+
+        return redirect(url_for(result,score=average_marks))
+
+         # return render_template('calculate.html',results=average_marks)
+
+if __name__=='__main__':
+ app.run(debug=True)
